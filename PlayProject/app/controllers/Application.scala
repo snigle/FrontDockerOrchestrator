e2d0cb6975b63e3a9d4e2a1058837769cf3b947d
Alexandre Etmezguine <alexandre.etmezguine@eisti.fr>
2015-11-16 17:09:13 +0100

    Version du login cookie qui marche

--- PlayProject/app/controllers/Application.scala
+++ PlayProject/app/controllers/Application.scala
@@ -1,12 +1,31 @@
 package controllers
 
-import play.api._
+import play.api.Play.current
+import play.api.libs.ws._
 import play.api.mvc._
+
+import scala.concurrent.ExecutionContext.Implicits.global
 
 class Application extends Controller {
 
-  def index = Action {
-    Ok(views.html.index("Your new application is ready."))
+  def index = getLogin
+
+
+
+  def getLogin = Action.async {
+
+    WS.url("https://vcloud-director-http-2.ccr.eisti.fr/api/login").withHeaders("Accept" -> "application/*+xml;version=5.1").withAuth("user1@icc-02", "eisti0002", WSAuthScheme.BASIC).get().map{
+      response => Ok(response.cookies.mkString)
+    }
+
+
   }
+
+
+
+
+
+
+
 
 }
