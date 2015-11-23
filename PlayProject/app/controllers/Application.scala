711cd1312f69516a8fee626ee32f4bbd0be376f4
Alexandre Etmezguine <alexandre.etmezguine@eisti.fr>
2015-11-23 18:48:11 +0100

    WS avec seq cookie

--- PlayProject/app/controllers/Application.scala
+++ PlayProject/app/controllers/Application.scala
@@ -8,22 +8,43 @@ import scala.concurrent.ExecutionContext.Implicits.global
 import scala.concurrent.Future
 
 class Application extends Controller {
-  var cookie : String =""
+  var cookie : Seq[WSCookie] = Seq[WSCookie]()
 
-  def index = Action.async { implicit request => {
+  def index = Action.async {
+    implicit request => {
      getLogin.onComplete(x => cookie = x.get )
-     Future(Ok(cookie))
-  }
-
+      val vcloud_token = cookie.head.toString.split(";")(0).split("=")(1)
+      Future(Ok(cookie.head.toString))
+    }
   }
 
 
 
   def getLogin = WS.url("https://vcloud-director-http-2.ccr.eisti.fr/api/login").withHeaders("Accept" -> "application/*+xml;version=5.1").withAuth("user1@icc-02", "eisti0002", WSAuthScheme.BASIC).get().map{
       response => {
-        response.cookies.mkString
+        response.cookies
       }
     }
+
+   /*def copieVM(cookie : String, source_vm : String, name_new_vm : String) = {
+    val vm_copy_xml =  <?xml version="1.0" encoding="UTF-8" standalone="yes"?>
+                           <RecomposeVAppParams
+                            xmlns="http://www.vmware.com/vcloud/v1.5"
+                            xmlns:ns2="http://schemas.dmtf.org/ovf/envelope/1"
+                            xmlns:xsi="http://www.w3.org/2001/XMLSchema-instance"
+                            xmlns:ovf="http://schemas.dmtf.org/ovf/envelope/1"
+                            xmlns:environment_1="http://schemas.dmtf.org/ovf/environment/1">
+                              <Description> "api deployed vm to ol-vapp-04" </Description>
+                              <SourcedItem sourceDelete="false">
+                                <Source name="tata" href="https://vcloud-director-http-2.ccr.eisti.fr/api/vApp/vm-4af381bb-f4d1-4784-8fc2-9f0db72411c9"/>
+                              </SourcedItem>
+                              <AllEULAsAccepted>true</AllEULAsAccepted>
+                          </RecomposeVAppParams>
+
+
+    WS.url("https://vcloud-director-http-2.ccr.eisti.fr/api/vApp/vapp-9dd013e3-3f51-4cde-a19c-f96b4ad2e350/action/recomposeVApp").withHeaders("Cookie" -> cookie).post(vm_copy_xml)
+  } */
+
 
 
   }
