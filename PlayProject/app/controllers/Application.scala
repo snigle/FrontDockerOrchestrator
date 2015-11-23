a0ae4727c8439812d3434ac71f6db022f98c2cd7
Alexandre Etmezguine <alexandre.etmezguine@eisti.fr>
2015-11-23 17:16:47 +0100

    cookie en string

--- PlayProject/app/controllers/Application.scala
+++ PlayProject/app/controllers/Application.scala
@@ -5,17 +5,24 @@ import play.api.libs.ws._
 import play.api.mvc._
 
 import scala.concurrent.ExecutionContext.Implicits.global
+import scala.concurrent.Future
 
 class Application extends Controller {
+  var cookie : String =""
 
-  def index = getLogin
+  def index = Action.async { implicit request => {
+     getLogin.onComplete(x => cookie = x.get )
+     Future(Ok(cookie))
+  }
+
+  }
 
 
 
-  def getLogin = Action.async {
-
-    WS.url("https://vcloud-director-http-2.ccr.eisti.fr/api/login").withHeaders("Accept" -> "application/*+xml;version=5.1").withAuth("user1@icc-02", "eisti0002", WSAuthScheme.BASIC).get().map{
-      response => Ok(response.cookies.mkString)
+  def getLogin = WS.url("https://vcloud-director-http-2.ccr.eisti.fr/api/login").withHeaders("Accept" -> "application/*+xml;version=5.1").withAuth("user1@icc-02", "eisti0002", WSAuthScheme.BASIC).get().map{
+      response => {
+        response.cookies.mkString
+      }
     }
 
 
@@ -28,4 +35,4 @@ class Application extends Controller {
 
 
 
-}
+
