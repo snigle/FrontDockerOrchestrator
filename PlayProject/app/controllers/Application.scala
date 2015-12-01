4d1dae3630d8116f1294dc926737189c75f779ec
Lamarche Ludovic <lamarchelu@eisti.eu>
2015-12-01 19:29:59 +0100

    Parse XML in the dashboard action, and synchronous cookie

--- PlayProject/app/controllers/Application.scala
+++ PlayProject/app/controllers/Application.scala
@@ -9,29 +9,49 @@ import scala.util.Success
 import scala.util.Failure
 import scala.concurrent.Await
 import scala.concurrent.duration.Duration
+import javax.inject.Inject
 
-class Application extends Controller {
-  var cookie : Seq[WSCookie] = Seq[WSCookie]()
+class Application @Inject() (ws: WSClient) extends Controller {
+  var cookie: Seq[WSCookie] = Seq[WSCookie]()
 
   def index = Action.async {
-    implicit request => {
-     val cookie = Await.ready(getLogin, Duration.Inf).value.get match {
-       case Success(x) => x
-       case Failure(x) => Nil
-     }
-      Future(Ok(cookie.head.toString))
+    implicit request =>
+      {
+        val cookie = getCookie
+        Future(Ok(cookie))
+      }
+  }
+
+  def dashboard = Action.async { implicit request =>
+    {
+      //"Accept:application/*+xml;version=1.5" -X GET https://vcloud-director-http-2.ccr.eisti.fr/api/vApp/vapp-cb109e5b-e457-450e-aff9-322cdd6181f6/productSections
+      ws.url("https://vcloud-director-http-2.ccr.eisti.fr/api/vApp/vapp-9dd013e3-3f51-4cde-a19c-f96b4ad2e350/").withHeaders(
+        "Cookie" -> getCookie,
+        "Accept" -> "application/*+xml;version=1.5").get().map(response => {
+          val ids = response.xml \ "Children" \ "Vm" 
+          println(ids.isEmpty)
+          println(ids.map(id=>id.attribute("id"))mkString("\n"))
+          Ok(response.xml)
+        })
+        
     }
   }
 
 
-
-  def getLogin = WS.url("https://vcloud-director-http-2.ccr.eisti.fr/api/login").withHeaders("Accept" -> "application/*+xml;version=5.1").withAuth("user1@icc-02", "eisti0002", WSAuthScheme.BASIC).get().map{
-      response => {
-        response.cookies
+  def getCookie = {
+    val req =
+      ws.url("https://vcloud-director-http-2.ccr.eisti.fr/api/login").withHeaders("Accept" -> "application/*+xml;version=5.1").withAuth("user1@icc-02", "eisti0002", WSAuthScheme.BASIC).get().map {
+        response =>
+          {
+            response.cookies
+          }
       }
+    Await.ready(req, Duration.Inf).value.get match {
+      case Success(x) => x.head.toString
+      case Failure(x) => throw new Exception
     }
-
-   /*def copieVM(cookie : String, source_vm : String, name_new_vm : String) = {
+  }
+  /*def copieVM(cookie : String, source_vm : String, name_new_vm : String) = {
     val vm_copy_xml =  <?xml version="1.0" encoding="UTF-8" standalone="yes"?>
                            <RecomposeVAppParams
                             xmlns="http://www.vmware.com/vcloud/v1.5"
@@ -50,9 +70,7 @@ class Application extends Controller {
     WS.url("https://vcloud-director-http-2.ccr.eisti.fr/api/vApp/vapp-9dd013e3-3f51-4cde-a19c-f96b4ad2e350/action/recomposeVApp").withHeaders("Cookie" -> cookie).post(vm_copy_xml)
   } */
 
-
-
-  }
+}
 
 
 
